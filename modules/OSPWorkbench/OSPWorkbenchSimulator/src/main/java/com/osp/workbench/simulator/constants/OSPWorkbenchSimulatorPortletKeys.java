335b2431edb94254de45fac09706a822fa2f30e9
DELL <DELL@121.153.140.126>
2018-11-28 19:19:00 +0900

    Home workbench simulator created

---
+++ modules/OSPWorkbench/OSPWorkbenchSimulator/src/main/java/com/osp/workbench/simulator/constants/OSPWorkbenchSimulatorPortletKeys.java
@@ -0,0 +1,10 @@
+package com.osp.workbench.simulator.constants;
+
+/**
+ * @author DELL
+ */
+public class OSPWorkbenchSimulatorPortletKeys {
+
+	public static final String OSPWorkbenchSimulator = "ospworkbenchsimulator";
+
+}
